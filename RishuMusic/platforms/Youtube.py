9e9f0d369448849d869b389d4aaf9870db06d2c1
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-11 15:33:13 +0530

    Update import paths from VISHALMUSIC to RishuMusic

--- RishuMusic/platforms/Youtube.py
+++ RishuMusic/platforms/Youtube.py
@@ -22,17 +22,17 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
 
-from VISHALMUSIC.utils.cookie_handler import COOKIE_PATH
-from VISHALMUSIC.utils.database import is_on_off
-from VISHALMUSIC.utils.downloader import download_audio_concurrent, yt_dlp_download
-from VISHALMUSIC.utils.errors import capture_internal_err
-from VISHALMUSIC.utils.formatters import time_to_seconds
-from VISHALMUSIC.utils.tuning import (
+from RishuMusic.utils.cookie_handler import COOKIE_PATH
+from RishuMusic.utils.database import is_on_off
+from RishuMusic.utils.downloader import download_audio_concurrent, yt_dlp_download
+from RishuMusic.utils.errors import capture_internal_err
+from RishuMusic.utils.formatters import time_to_seconds
+from RishuMusic.utils.tuning import (
     YTDLP_TIMEOUT,
     YOUTUBE_META_MAX,
     YOUTUBE_META_TTL,
 )
-from VISHALMUSIC import LOGGER
+from RishuMusic import LOGGER
 
 _module_logger = LOGGER(__name__)
 
