df94e7712fa43e94aca4d84fe45d64f92bd16e9c
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-11 16:51:29 +0530

    Update Youtube.py

--- RishuMusic/platforms/Youtube.py
+++ RishuMusic/platforms/Youtube.py
@@ -20,16 +20,16 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 
-from VIPMUSIC.utils.cookie_handler import COOKIE_PATH
-from VIPMUSIC.utils.database import is_on_off
-from VIPMUSIC.utils.errors import capture_internal_err
-from VIPMUSIC.utils.formatters import time_to_seconds
-from VIPMUSIC.utils.tuning import (
+from RishuMusic.utils.cookie_handler import COOKIE_PATH
+from RishuMusic import is_on_off
+from RishuMusic.utils.errors import capture_internal_err
+from RishuMusic.utils.formatters import time_to_seconds
+from RishuMusic.utils.tuning import (
     YTDLP_TIMEOUT,
     YOUTUBE_META_MAX,
     YOUTUBE_META_TTL,
 )
-from VIPMUSIC import LOGGER
+from RishuMusic import LOGGER
 
 _module_logger = LOGGER(__name__)
 
